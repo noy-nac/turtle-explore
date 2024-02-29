1bf771c883291c8fbf2cf9770752e3fbaf82904d
Canyon Mooney <canyon.mooney@gmail.com>
2024-02-29 08:57:30 -0600

    starter code

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+screen = Screen()
+
+t = Turtle()
+
+t.forward(100)
+
+# TODO draw a picture
+# TODO write your name
+# TODO try new functions
+
+# Look at these docs to find more functions
+# https://docs.python.org/3/library/turtle.html
+
+screen.mainloop()
